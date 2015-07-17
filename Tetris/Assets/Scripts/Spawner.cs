6e9b93fe53b13c65a2313b9d9939dbf17f38b955
Yamil1293 <yamil.burgos1293@gmail.com>
2015-07-16 23:35:30 -0400

    All the assets for Tetris

---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] groups;
+
+	public void spawnNext()
+	{
+		int i = Random.Range (0, groups.Length);
+
+		Instantiate (groups [i], transform.position, Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		spawnNext ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
